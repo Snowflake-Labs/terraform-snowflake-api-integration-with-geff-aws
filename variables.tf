ef631bbca6b9b91dbcb98d1a2be8eb51b441156d
Chetan <chetan.nakhate@snowflake.com>
2021-04-22 22:30:51 +0530

    Initial commit

--- variables.tf
+++ variables.tf
@@ -1,11 +1,45 @@
-variable "prefix" {}
-variable "snowflake_integration_user" {}
-variable "aws_cloudwatch_metric_namespace" {}
-variable "aws_deployment_stage_name" {}
-variable "snowflake_username" {}
-variable "snowflake_account" {}
-variable "snowflake_password" {}
-variable "snowflake_role" {}
+variable "prefix" {
+  type        = string
+  description = "this will be the prefix used to name the Resources"
+  default     = "example"
+}
+
+variable "snowflake_integration_user" {
+  type        = string
+  description = "user who will be calling the API Gateway"
+  default     = null
+}
+
+variable "aws_cloudwatch_metric_namespace" {
+  type        = string
+  description = "where EF can write CloudWatch Metrics"
+  default     = "*"
+}
+
+variable "aws_deployment_stage_name" {
+  type        = string
+  default     = "prod"
+  description = "AWS stage name the Snowflake user will assume to deploy the API Gateway in your account"
+}
+
+variable "snowflake_account" {
+  type        = string
+}
+
+variable "snowflake_role" {
+  type        = string
+  default     = "ACCOUNTADMIN"
+}
+
+variable "snowflake_username" {
+  type        = string
+  sensitive   = true
+}
+
+variable "snowflake_password" {
+  type        = string
+  sensitive   = true
+}
 
 
 data "aws_caller_identity" "current" {}
