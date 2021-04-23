dde4c791aec8867c1cf11fb7c9b5c7c15b63f1dc
Chetan <chetan.nakhate@snowflake.com>
2021-04-23 23:00:36 +0530

    added outputs.tf file in example

---
+++ example/output.tf
@@ -0,0 +1,11 @@
+output "api_integration_name"{
+    type        = string
+    description = "Name of API integration"
+    value       = "${var.prefix}_api_integration"
+}
+
+output "api_gateway_invoke_url"{
+    type        = list
+    description = "List of all invoked url"
+    value       = aws_api_gateway_deployment.prod.invoke_url
+}
