8846c0170be43325d7ebf7e954a18160091983af
Chetan <chetan.nakhate@snowflake.com>
2021-04-23 22:59:14 +0530

    added outputs

--- output.tf
+++ output.tf
@@ -1,9 +1,10 @@
-variable api_integration_name{
+output "api_integration_name"{
     type        = string
     description = "Name of API integration"
     value       = "${var.prefix}_api_integration"
 }
-variable api_gateway_invoke_url{
+
+output "api_gateway_invoke_url"{
     type        = list
     description = "List of all invoked url"
     value       = aws_api_gateway_deployment.prod.invoke_url
