94c5eb4689b98114a68a49ab3382485ec1fc31e5
Rutuja <rutuja.gaikwad@snowflake.com>
2021-04-23 22:30:54 +0530

    output file

---
+++ output.tf
@@ -0,0 +1,8 @@
+variable api_integration_name{
+    type = string
+    description = "Name of API integration"
+}
+variable api_gateway_invoke_url{
+    type = list
+    description = "List of all invoked url"
+}
