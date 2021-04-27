c2ee8da29cadd172f25e1d5584435020912cfa6e
Chetan <chetan.nakhate@snowflake.com>
2021-04-27 22:23:53 +0530

    made some changes in output.tf

--- example/output.tf
+++ example/output.tf
@@ -1,11 +1,9 @@
 output "api_integration_name"{
-    # type        = string
     description = "Name of API integration"
     value       = "${var.prefix}_api_integration"
 }
 
 output "api_gateway_invoke_url"{
-    # type        = list
     description = "List of all invoked url"
     value       = module.snowflake_api_integration_aws_gateway.api_gateway_invoke_url
 }
