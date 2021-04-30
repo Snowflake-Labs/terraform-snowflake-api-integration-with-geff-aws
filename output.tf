ba3b6f25db71aadee4a560a0752dafea1f333931
sfc-gh-afedorov <42752788+sfc-gh-afedorov@users.noreply.github.com>
2021-04-29 20:18:32 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "api_integration_name"{
+output "api_integration_name" {
     description = "Name of API integration"
     value       = "${var.prefix}_api_integration"
 }
 
-output "api_gateway_invoke_url"{
+output "api_gateway_invoke_url" {
     description = "List of all invoked url"
     value       = aws_api_gateway_deployment.prod.invoke_url
 }
