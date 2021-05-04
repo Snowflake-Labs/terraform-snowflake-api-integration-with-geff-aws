1e07f24fa2722e828a7a147d838b95afeed52a8c
Prasanth <72515998+sfc-gh-pkommini@users.noreply.github.com>
2021-05-04 11:59:46 +0530

    Refactor terraform logs and consistent var names

---
+++ examples/complete/versions.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.37.0"
+    }
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.0"
+    }
+  }
+}
+
+provider "snowflake" {
+  username = var.snowflake_username
+  account  = var.snowflake_account
+  password = var.snowflake_password
+  role     = var.snowflake_role
+}
+
+provider "aws" {
+  region = var.aws_region
+}
