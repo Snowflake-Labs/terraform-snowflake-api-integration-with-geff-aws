b4c9815364c7e30c789bba42c1eade94398a5a29
Prasanth <72515998+sfc-gh-pkommini@users.noreply.github.com>
2021-05-04 12:03:33 +0530

    Add example

--- api_integration.tf
+++ api_integration.tf
@@ -1,9 +1,8 @@
-
 resource "snowflake_api_integration" "api_integration" {
   name                 = "${var.prefix}_api_integration"
   enabled              = true
   api_provider         = "aws_api_gateway"
-  api_allowed_prefixes = [aws_api_gateway_deployment.prod.invoke_url]
+  api_allowed_prefixes = [aws_api_gateway_deployment.geff.invoke_url]
   api_aws_role_arn     = "arn:aws:iam::${local.account_id}:role/${var.prefix}-api-gateway-caller"
 }
 
--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
+variable "aws_region" {
+  description = "The AWS region in which the AWS infrastructure is created."
+  default     = "us-west-2"
+}
+
 variable "prefix" {
   type        = string
   description = "this will be the prefix used to name the Resources"
   default     = "example"
-}
-
-variable "aws_region" {
-  type        = string
-  default     = "us-west-2"
 }
 
 variable "aws_cloudwatch_metric_namespace" {
@@ -15,30 +15,35 @@ variable "aws_cloudwatch_metric_namespace" {
   default     = "*"
 }
 
-variable "aws_deployment_stage_name" {
+variable "log_retention_days" {
+  description = "Log retention period in days."
+  default     = 0
+}
+
+variable "env" {
   type        = string
-  default     = "prod"
-  description = "AWS API Gateway deployment stage name"
+  default     = "dev"
+  description = "Dev/Prod/Staging or any other custom environment name."
 }
 
 variable "snowflake_account" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "snowflake_role" {
-  type        = string
-  default     = "ACCOUNTADMIN"
+  type    = string
+  default = "ACCOUNTADMIN"
 }
 
 variable "snowflake_username" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "snowflake_password" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 
