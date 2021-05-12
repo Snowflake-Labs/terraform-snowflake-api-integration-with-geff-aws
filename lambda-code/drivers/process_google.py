4c85b2c4d210fbc155458ac83c4d6f8d2a809c6d
Andrey Fedorov <andrey.fedorov@snowflake.com>
2021-05-11 18:44:55 -0700

    Update process_google.py
    add ability to use authorized_user_info (e.g. OAuth delegated Calendar access)

--- lambda-code/drivers/process_google.py
+++ lambda-code/drivers/process_google.py
@@ -1,29 +1,36 @@
 from json import loads
 
-from google.oauth2 import service_account
+from google.oauth2 import service_account, credentials
 from googleapiclient.discovery import build
 
 from vault import decrypt_if_encrypted
 
 
 def process_row(
-    service_account_info,
     service_name,
     service_version,
     resource_name,
     method,
     params,
+    service_account_info=None,
+    authorized_user_info=None,
     without_subject=False,
     subject=None,
     scopes=None,
 ):
-    c = service_account.Credentials.from_service_account_info(
-        loads(decrypt_if_encrypted(service_account_info))
-    )
-    if subject is None or without_subject:
-        creds = c.with_scopes(scopes)
+    if authorized_user_info is not None:
+        creds = credentials.Credentials.from_authorized_user_info(
+            loads(decrypt_if_encrypted(authorized_user_info)), scopes
+        )
+
     else:
-        creds = c.with_subject(subject).with_scopes(scopes)
+        c = service_account.Credentials.from_service_account_info(
+            loads(decrypt_if_encrypted(service_account_info))
+        )
+        if subject is None or without_subject:
+            creds = c.with_scopes(scopes)
+        else:
+            creds = c.with_subject(subject).with_scopes(scopes)
 
     service = build(service_name, version=service_version, credentials=creds)
     resource_name, *subresource_names = resource_name.split('.')
