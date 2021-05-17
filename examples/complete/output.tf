34e74421ee364c0aa9f4396d825cb6f54eec8794
Prasanth <72515998+sfc-gh-pkommini@users.noreply.github.com>
2021-05-17 15:39:45 +0530

    Fix order of outputs.tf to priority of resources needed for snowflake development

--- examples/complete/output.tf
+++ examples/complete/output.tf
@@ -1,11 +1,16 @@
-output "api_integration_name" {
-  description = "Name of API integration"
-  value       = module.snowflake_api_integration_aws_gateway.api_integration_name
-}
-
 output "api_gateway_invoke_url" {
   description = "List of all invoked url"
   value       = module.snowflake_api_integration_aws_gateway.api_gateway_invoke_url
+}
+
+output "allowed_prefixes" {
+  description = "This is the inferred API Gateway invoke URL which we use as allowed prefixes."
+  value       = module.snowflake_api_integration_aws_gateway.allowed_prefixes
+}
+
+output "api_integration_name" {
+  description = "Name of API integration"
+  value       = module.snowflake_api_integration_aws_gateway.api_integration_name
 }
 
 output "storage_integration_name" {
@@ -16,9 +21,4 @@ output "storage_integration_name" {
 output "bucket_url" {
   description = "GEFF S3 Bucket URL"
   value       = module.snowflake_api_integration_aws_gateway.bucket_url
-}
-
-output "allowed_prefixes" {
-  description = "This is the inferred API Gateway invoke URL which we use as allowed prefixes."
-  value       = module.snowflake_api_integration_aws_gateway.allowed_prefixes
 }
--- output.tf
+++ output.tf
@@ -1,11 +1,16 @@
-output "api_integration_name" {
-  description = "Name of API integration"
-  value       = snowflake_api_integration.geff_api_integration.name
-}
-
 output "api_gateway_invoke_url" {
   description = "List of all invoked url"
   value       = aws_api_gateway_deployment.geff_api_gw_deployment.invoke_url
+}
+
+output "allowed_prefixes" {
+  description = "This is the inferred API Gateway invoke URL which we use as allowed prefixes."
+  value       = local.inferred_api_gw_invoke_url
+}
+
+output "api_integration_name" {
+  description = "Name of API integration"
+  value       = snowflake_api_integration.geff_api_integration.name
 }
 
 output "storage_integration_name" {
@@ -16,9 +21,4 @@ output "storage_integration_name" {
 output "bucket_url" {
   description = "GEFF S3 Bucket URL"
   value       = "s3://${aws_s3_bucket.geff_bucket.id}/${aws_s3_bucket_object.geff_data_folder.id}"
-}
-
-output "allowed_prefixes" {
-  description = "This is the inferred API Gateway invoke URL which we use as allowed prefixes."
-  value       = local.inferred_api_gw_invoke_url
 }
