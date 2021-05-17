a07acfc7cc9883e08ecbdd396dcb745266a2897a
Prasanth <72515998+sfc-gh-pkommini@users.noreply.github.com>
2021-05-17 13:48:13 +0530

    Fix logging

--- examples/complete/output.tf
+++ examples/complete/output.tf
@@ -1,6 +1,6 @@
 output "api_integration_name" {
   description = "Name of API integration"
-  value       = "${var.prefix}_api_integration"
+  value       = module.snowflake_api_integration_aws_gateway.api_integration_name
 }
 
 output "api_gateway_invoke_url" {
@@ -10,7 +10,7 @@ output "api_gateway_invoke_url" {
 
 output "storage_integration_name" {
   description = "Name of Storage integration"
-  value       = "${var.prefix}-geff-storage-integration"
+  value       = module.snowflake_api_integration_aws_gateway.storage_integration_name
 }
 
 output "bucket_url" {
