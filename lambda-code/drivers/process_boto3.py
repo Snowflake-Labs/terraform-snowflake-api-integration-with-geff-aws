8d42047c25753124ebcfc02b84d01b54678491bb
Andrey Fedorov <andrey.fedorov@snowflake.com>
2021-06-10 19:53:46 -0700

    Update process_boto3.py

--- lambda-code/drivers/process_boto3.py
+++ lambda-code/drivers/process_boto3.py
@@ -1,11 +1,23 @@
+from json import loads
+
 import boto3
+from botocore.response import StreamingBody
 
 
 DISALLOWED_CLIENTS = {'kms', 'secretsmanager'}
 
 
-def process_row(client_name, method_name, region='us-west-2', **kwargs):
+def process_row(
+    client_name, method_name, results_path=None, region='us-west-2', **kwargs
+):
     if client_name in DISALLOWED_CLIENTS:
         return
     method = getattr(boto3.client(client_name, region), method_name)
-    return method(**kwargs)
+    result = method(**kwargs)
+    if (
+        results_path
+        and result.get('ContentType') == 'application/json'
+        and isinstance(result.get('Body'), StreamingBody)
+    ):
+        result = pick(results_path, loads(result.get('Body')).read())
+    return result
