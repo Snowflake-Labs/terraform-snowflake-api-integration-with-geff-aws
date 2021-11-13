51be6ce1816d9584a9a2f2c3a0235d50f5c7ec66
Prasanth Kommini <prasanth.kommini@snowflake.com>
2021-11-13 09:30:40 -0800

    Add variable to input integration user roles to which we grant usage permissions

--- api_integration.tf
+++ api_integration.tf
@@ -5,3 +5,12 @@ resource "snowflake_api_integration" "geff_api_integration" {
   api_allowed_prefixes = [local.inferred_api_gw_invoke_url]
   api_aws_role_arn     = "arn:aws:iam::${local.account_id}:role/${local.api_gw_caller_role_name}"
 }
+
+resource "snowflake_integration_grant" "geff_api_integration_grant" {
+  integration_name = snowflake_api_integration.geff_api_integration.name
+
+  privilege = "USAGE"
+  roles     = var.snowflake_integration_user_roles
+
+  with_grant_option = false
+}
