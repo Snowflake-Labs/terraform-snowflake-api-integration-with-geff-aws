f2f23c7d627ac6fad746be3ee062c4307baf2387
sfc-gh-amathew <77770112+sfc-gh-amathew@users.noreply.github.com>
2021-12-11 00:15:49 +0530

    Update storage_integration.tf

--- storage_integration.tf
+++ storage_integration.tf
@@ -2,7 +2,7 @@
 
 
 module "gsif" {
-  source                           = "git::https://github.com/Snowflake-Labs/terraform-snowflake-storage-integration.git?ref=add-changes"
+  source                           = "git::https://github.com/Snowflake-Labs/terraform-snowflake-storage-integration.git"
   snowflake_account                = var.snowflake_account
   snowflake_integration_user_roles = var.snowflake_integration_user_roles
 
