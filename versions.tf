5006d05ff6db5423cf23c2a2c9fcb89b46e25c56
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-06-29 18:51:46 -0700

    Switch snowflake provider to snowflake-labs instead of czi.

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.1.7"
+  required_version = ">= 1.2.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.2.0"
+      version = "4.20.1"
     }
 
     snowflake = {
@@ -18,3 +18,4 @@ terraform {
     }
   }
 }
+
