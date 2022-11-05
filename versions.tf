fbb6800edc7da53c564c270bbf7fd2244f0252ea
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-11-04 18:20:58 -0700

    Bump versions.

--- versions.tf
+++ versions.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = ">= 1.2.0"
+  required_version = "~> 1.3.4"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.20.1"
+      version = "~> 4.37.0"
     }
 
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = ">= 0.40.0"
+      version = "~> 0.40.0"
 
       configuration_aliases = [
         snowflake.api_integration_role,
