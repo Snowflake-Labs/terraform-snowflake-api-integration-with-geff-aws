d7939620a773ce34089f1b02e637c968509794e1
sfc-gh-nlele <nachiket.lele@snowflake.com>
2023-05-02 22:24:40 +0530

    Update dynamodb naming conditions

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "geff_batch_locking_table" {
   count        = var.create_dynamodb_table ? 1 : 0
-  name         = "${local.geff_prefix}_batch_locking_table"
+  name         = var.user_managed_dynamodb_table_name != null ? var.user_managed_dynamodb_table_name : "${local.geff_prefix}_batch_locking_table" 
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "batch_id"
 
@@ -13,18 +13,14 @@ resource "aws_dynamodb_table" "geff_batch_locking_table" {
     attribute_name = "ttl"
     enabled        = true
   }
-
-  tags = {
-    name = "${local.geff_prefix}_batch_locking_table"
-  }
 }
 
 data "aws_dynamodb_table" "user_managed_table" {
-  count = var.user_managed_dynamodb_table_name != null ? 1 : 0
+  count = var.user_managed_dynamodb_table_name != null && !var.create_dynamodb_table ? 1 : 0
   name  = var.user_managed_dynamodb_table_name
 }
 
 locals {
-  dynamodb_table_name = var.user_managed_dynamodb_table_name != null ? data.aws_dynamodb_table.user_managed_table[0].name : (var.create_dynamodb_table ? aws_dynamodb_table.geff_batch_locking_table[0].name : null)
-  dynamodb_table_arn = var.user_managed_dynamodb_table_name != null ? data.aws_dynamodb_table.user_managed_table[0].arn : (var.create_dynamodb_table ? aws_dynamodb_table.geff_batch_locking_table[0].arn : null)
+  dynamodb_table_name = var.user_managed_dynamodb_table_name != null ? (var.create_dynamodb_table ? aws_dynamodb_table.geff_batch_locking_table[0].name : data.aws_dynamodb_table.user_managed_table[0].name) : (var.create_dynamodb_table ?  aws_dynamodb_table.geff_batch_locking_table[0].name : null)
+  dynamodb_table_arn = var.user_managed_dynamodb_table_name != null ? (var.create_dynamodb_table ? aws_dynamodb_table.geff_batch_locking_table[0].arn : data.aws_dynamodb_table.user_managed_table[0].arn) : (var.create_dynamodb_table ?  aws_dynamodb_table.geff_batch_locking_table[0].arn : null)
 }
