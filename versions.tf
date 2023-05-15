ff8e2ff210ecf39db4c7ad6406652d149774257d
Nachiket <77716642+sfc-gh-nlele@users.noreply.github.com>
2023-05-15 21:09:55 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -9,7 +9,7 @@ terraform {
 
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.57.0"
+      version = ">= 0.62.0"
 
       configuration_aliases = [
         snowflake.api_integration_role,
