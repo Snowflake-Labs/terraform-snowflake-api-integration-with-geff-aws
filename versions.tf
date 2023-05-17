5ec976050eb7ab4caa26181f4741ca6cc763ab4d
Bora Kou <bora.kou@snowflake.com>
2023-05-17 11:17:14 -0700

    Upgrade Snowflake Terraform provider (#79)

--- versions.tf
+++ versions.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = "~> 1.3.4"
+  required_version = ">= 1.3.4"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.38.0"
+      version = ">= 4.38.0"
     }
 
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.57.0"
+      version = ">= 0.62.0"
 
       configuration_aliases = [
         snowflake.api_integration_role,
