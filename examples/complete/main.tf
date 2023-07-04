3b332a4423ac2f83a695efae92e8dfebe0300125
sfc-gh-nlele <nachiket.lele@snowflake.com>
2023-07-04 18:20:05 +0530

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -20,9 +20,13 @@ module "geff" {
   snowflake_integration_user_roles = var.snowflake_integration_user_roles
   geff_secret_arns                 = var.geff_secret_arns
 
-  create_batch_locking_table    = var.create_batch_locking_table
-  batch_locking_table_name = var.batch_locking_table_name
-  batch_locking_table_ttl       = var.batch_locking_table_ttl
+  create_batch_locking_table = var.create_batch_locking_table
+  batch_locking_table_name   = var.batch_locking_table_name
+  batch_locking_table_ttl    = var.batch_locking_table_ttl
+
+  create_rate_limiting_table = var.create_rate_limiting_table
+  rate_limiting_table_name   = var.rate_limiting_table_name
+  rate_limiting_table_ttl    = var.batch_locking_table_ttl
 
   providers = {
     snowflake.api_integration_role     = snowflake.api_integration_role
